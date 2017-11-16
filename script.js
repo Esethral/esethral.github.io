f39259beacbd5c99fb905b97cbabf7c9399066b9
Esethral <eabel@stevens.edu>
2017-11-15 23:14:15 -0500

    Animation testing
    Chnaging menus with JQuery animations

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-function hide(){
-	document.getElementById("myP").style.visibility = "hidden"
+//E Logo Color Change
+$(document).ready( function() {
+    $( ".sidebar" ).mouseenter(function() {
+      $(".e").css("filter", "grayscale(0%)");
+    });
+    $( ".sidebar" ).mouseleave(function() {
+      $(".e").css("filter", "grayscale(100%)");
+    });
+  } );
+//Sidebar Icons Fade
+$(document).ready(function(){
+    $(".sidebar").mouseenter(function(){
+        $(".github").fadeTo(500, 1);
+        $(".linkedin").fadeTo(500, 1);
+    });
+    $(".sidebar").mouseleave(function(){
+        $(".github").fadeTo(500, 0);
+        $(".linkedin").fadeTo(500, 0);
+    });
+});
+//About Section Page Layout Change
+function slideAbout(){
+	$(".cellAbout").css("height", "10%");
+	$(".cellProjects").css("height", "10%");
+	$(".cellGames").css("height", "10%");
+	$(".textAbout").slideToggle("2000");
+	$(".textAbout").css("height", "70%");
 }
-function about(){
-	
-}
+
+$(document).ready(function(){
+    $(".about").click(function(){
+        $(".cellAbout").css("height", "10%");
+        $(".cellProjects").css("height", "45%");
+        $(".cellGames").css("height", "45%");
+    });
+    $(".about").click(function(){
+    	setTimeout(slideAbout, 2000);
+    });
+});
